c4a71502a523ee21c60aa00dec9c10072be3ddad
AmingYee <113171179+AmingYee@users.noreply.github.com>
2023-02-06 12:07:18 +0000

    done done

---
+++ .github/fix-samples.sh
@@ -0,0 +1,6 @@
+cd ./samples
+ mv ./animals/countries/movies/ ./
+ mv ./animals/countries/ ./
+ mv ./animals/Monkies.txt ./animals/Monkeys.txt
+ mv ./movies/Germany.txt ./countries/Germany.txt
+ mv ./countries/.apes.txt ./animals/.apest.txt
